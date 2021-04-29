d2efddee68246f113cba81fb6e1676a0230c03e7
Emilse <emilsearias@gmail.com>
2021-04-29 11:33:32 -0300

    se agrego la cadena de conexion a la bd

---
+++ src/database.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const url='mongodb://localhost:27017/cafeteria5a';
+
+mongoose.connect(url,{useNewUrlParser: true, useUnifiedTopology: true});
+
+// guardar la conexion en una variable
+
+const connection = mongoose.connection;
+
+connection.once('open',()=>{
+    console.log('BD conectada');
+})
