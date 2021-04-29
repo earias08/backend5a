c855d31ae105cb739e7581efe0383c685e4da196
Emilse <emilsearias@gmail.com>
2021-04-29 10:41:22 -0300

    se agrego la configuracion inicial del proyecto

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-// const express = requiere('express');
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import path from 'path';
+
+// settings
+// creo una instancia de express;
+const app = express();
+
+// const port = 4000;
+app.set('port', process.env.PORT || 4000);
+
+app.listen(app.get('port') ,()=>{
+    // console.log(path.join(__dirname,'../public'))
+    console.log('Estoy en el puerto ' + app.get('port'));
+});
+
+//Middlewares
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+// agregaro la carpeta public como estatica
+app.use(express.static(path.join(__dirname,'../public')))
+
+// crear una ruta
+app.get('/',(req,res)=>{
+    res.send('Hola desde el servidor de backend');
+});
+
