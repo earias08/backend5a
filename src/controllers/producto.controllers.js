8a38860410f67d7c6c6a59e0819938a39ee881aa
Emilse <emilsearias@gmail.com>
2021-05-06 10:44:32 -0300

    se agrego la ruta y el controller para agregar Productos

--- src/controllers/producto.controllers.js
+++ src/controllers/producto.controllers.js
@@ -1,3 +1,5 @@
+import Producto from '../models/producto';
+
 // creo un objeto vacio que tendra la logica del backend
 const cafeteriaCtrl = {};
 
@@ -5,9 +7,30 @@ cafeteriaCtrl.getPrueba = (req, res)=>{
     res.send('prueba desde el controlador')
 }
 
-cafeteriaCtrl.crearProducto =(req, res)=>{
+cafeteriaCtrl.crearProducto =async(req, res)=>{
     console.log(req.body);
-    res.send('desde crearProducto controller')
+    try{
+        const {nombreProducto, precioProducto, categoria} = req.body
+        // validar los datos
+        // crear un objeto en la BD
+        const productoNuevo = new Producto({
+            nombreProducto: nombreProducto,
+            precioProducto: precioProducto,
+            categoria: categoria
+        });
+        // guardar el nuevo producto
+        await productoNuevo.save();
+        // enviar la respuesta al frontend
+        res.status(201).json({
+            mensaje: 'Producto agregado a la BD'
+        });
+    }catch(error){
+        console.log(error);
+        // enviar codigo de error
+        res.status(500).json({
+            mensaje: 'Ocurrio un error al intentar guardar los datos'
+        })
+    }
 }
 
 export default cafeteriaCtrl;
